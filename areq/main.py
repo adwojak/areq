aa28e871fef15c381a168dd54c2a3b320f709613
adwojak <dwojaka207@gmail.com>
2022-01-05 21:50:04 +0100

    Simple example of aiohttp

---
+++ areq/main.py
@@ -0,0 +1,17 @@
+import aiohttp
+import asyncio
+
+
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://www.google.com') as response:
+
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            html = await response.text()
+            print("Body:", html[:15], "...")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
